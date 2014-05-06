7ed62ff694fdcaf688d2007e30e0216a82be5cf3
Matt Nish <matt.nish@gmail.com>
2014-05-06 16:24:59 -0700

    updated styling

--- app.js
+++ app.js
@@ -14,18 +14,27 @@
   };
   
   var SomethingCool = function(element,options){
-    var bgColorsArray   = options.bgColors;
-    this.innerText      = $(element).text();
+    var bgColorsArray   = options.bgColors,
+        date            = new Date(),
+        hours           = date.getHours(),
+        minutes         = date.getMinutes();
+    this.timeString     = this.translateTime(hours,minutes);
     this.totalBGs       = bgColorsArray.length;
     this.countInArray   = 0;
     this.$el            = $(element);
     
     this.$el.css('background-color', bgColorsArray[this.countInArray]);
+    this.$el.find('.cool-time').text(this.timeString);
+    
     var nextFunc = function(){
         setTimeout(function(){
+          var date            = new Date(),
+              hours           = date.getHours(),
+              minutes         = date.getMinutes();
+          this.timeString     = this.translateTime(hours,minutes);
           this.nextThing(this.nextColor(bgColorsArray));
           nextFunc();
-        }.bind(this), 4000);
+        }.bind(this), 4100);
     }.bind(this);
     nextFunc();
   }
@@ -39,7 +48,7 @@
       //console.log('testing', this.$el);
       var $div = ('<div class="cool-container upcoming" style="background-color:'+nextBG+'">'+
                       '<div class="cool-wrap">'+
-                        this.innerText +
+                        this.timeString +
                       '</div>'+
                     '</div>');
       this.$el.after($div);
@@ -49,8 +58,24 @@
       setTimeout(function(){
         this.$el = this.$el.parent().find('.cool-container.upcoming');
         this.$el.removeClass('upcoming').prev().remove();
-      }.bind(this),2100);
+      }.bind(this),4100);
       
+    },
+    translateTime: function(hours, minutes){
+      var isPM = false;
+      if(hours >= 12){
+        if(hours !== 24){
+          isPM = true;
+        }
+        if(hours !== 12){
+          hours -= 12;
+        }
+      }
+      if((minutes + '').length == 1){
+        minutes = 0 + '' + minutes;
+      }
+      
+      return (isPM) ? (hours + ":" + minutes + " PM") : (hours + ":" + minutes + " AM");
     }
   }
   
